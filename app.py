a7d325f65b5ff0ec04085594b84a323863676483
Priyanshu Kumar <priyanshu6202018611@gmail.com>
2026-09-19 04:38:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "project": "AgriVision AI",
+        "status": "running",
+        "message": "AI Plant Disease Detection System"
+    })
+
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
